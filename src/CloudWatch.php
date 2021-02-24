e370bbdd3910e54fab0ace13104dc90edf72fbe6
James Stormes <jstormes@stormes.net>
2021-02-24 10:37:24 -0600

    Creating the package

---
+++ src/CloudWatch.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jstormes
+ * Date: 2/24/2021
+ * Time: 2:57 PM
+ */
+
+declare(strict_types=1);
+
+namespace JStormes\CloudWatch;
+
+use Aws\CloudWatchLogs\CloudWatchLogsClient;
+
+class CloudWatch
+{
+
+}
